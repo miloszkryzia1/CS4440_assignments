b755bd07ba44c62d3aae17f07ecaa45c288dfb90
Milosz Kryzia <milosz.kryzia@gmail.com>
2025-09-09 18:31:56 -0700

    complete similarity.py

--- assignment_1/similarity.py
+++ assignment_1/similarity.py
@@ -1,9 +1,9 @@
 # -------------------------------------------------------------------------
-# AUTHOR: your name
-# FILENAME: title of the source file
-# SPECIFICATION: description of the program
+# AUTHOR: Milosz Kryzia
+# FILENAME: similarity.py
+# SPECIFICATION: This program calculates the cosine similarity between text documents.
 # FOR: CS 5990 (Advanced Data Mining) - Assignment #1
-# TIME SPENT: how long it took you to complete the assignment
+# TIME SPENT: 
 # -----------------------------------------------------------*/
 
 #IMPORTANT NOTE: DO NOT USE ANY ADVANCED PYTHON LIBRARY TO COMPLETE THIS CODE SUCH AS numpy or pandas.
@@ -25,14 +25,35 @@ with open('cleaned_documents.csv', 'r') as csvfile:
 #Building the document-term matrix by using binary encoding.
 #You must identify each distinct word in the collection using the white space as your character delimiter.
 #--> add your Python code here
+vocab = set()
+for doc in documents:
+    words = doc[1].split()
+    vocab.update(words)
+
 docTermMatrix = []
 
+for doc in documents:
+    doc_vector = [1 if word in doc[1].split() else 0 for word in vocab]
+    docTermMatrix.append(doc_vector)
 
 # Compare the pairwise cosine similarities and store the highest one
 # Use cosine_similarity([X], [Y]) to calculate the similarities between 2 vectors
 # --> Add your Python code here
+similarities = {}
+max_similarity = 0
+most_similar_pair = None
+
+for i in range(len(docTermMatrix)):
+    for j in range(i + 1, len(docTermMatrix)):
+        sim = cosine_similarity([docTermMatrix[i]], [docTermMatrix[j]])[0][0]
+        similarities[(i+1, j+1)] = sim
+        if sim > max_similarity:
+            max_similarity = sim
+            most_similar_pair = (i+1, j+1)
 
 
 # Print the highest cosine similarity following the information below
 # The most similar documents are document 10 and document 100 with cosine similarity = x
 # --> Add your Python code here
+if most_similar_pair:
+    print(f"The most similar documents are document {most_similar_pair[0]} and document {most_similar_pair[1]} with cosine similarity = {max_similarity}")
